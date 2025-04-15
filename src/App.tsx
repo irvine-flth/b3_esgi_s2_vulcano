7933a56050b38cd9c93731742c61b4bbc50492e7
Irvine <fleith.irvine@gmail.com>
2025-04-15 11:52:42 +0200

    Refacto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import WorldMap from "./components/WorldMap";
 
 const App = () => {
-  const position = [51.505, -0.09];
-  return (
-    <MapContainer
-      center={position}
-      zoom={5}
-      scrollWheelZoom={true}
-      style={{ minHeight: "100vh", minWidth: "100vw" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
+  return <WorldMap />;
 };
 
 export default App;
