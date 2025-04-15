f2a0274e921849189eada1c43a6c7d2b87ec01bf
Irvine <fleith.irvine@gmail.com>
2025-04-15 16:34:19 +0200

    Leaflet imgs

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "leaflet/dist/leaflet.css"; // <- Leaflet styles
+import L from "leaflet";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL(
+    "leaflet/dist/images/marker-icon-2x.png",
+    import.meta.url
+  ).href,
+  iconUrl: new URL("leaflet/dist/images/marker-icon.png", import.meta.url).href,
+  shadowUrl: new URL("leaflet/dist/images/marker-shadow.png", import.meta.url)
+    .href,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
